8b34b4345fede06ddf54d4f1e8be65e59fb2b2e4
komix <oleh.petrovskyi@gmail.com>
2015-01-18 15:49:08 +0200

    jquery isotope

--- app/portfolio/portfolio.js
+++ app/portfolio/portfolio.js
@@ -9,8 +9,33 @@ angular.module('exel.portfolio', ['ngRoute'])
   });
 }])
 
-.controller('PortfolioController', ['$scope', '$http',function($scope, $http) {
+.controller('PortfolioController', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
 $http.get('portfolio/portfolio.json').success(function(data) {
     $scope.portfolio = data;
-  });
+
+});
+
+
+
+			function init() {
+				
+				$('.isotope-container').fadeIn();
+				var $container = $('.isotope-container').isotope({
+					itemSelector: '.isotope-item',
+					layoutMode: 'masonry',
+					transitionDuration: '0.6s',
+					filter: "*"
+				});
+				// filter items on button click
+				$('.filters').on( 'click', 'ul.nav li a', function() {
+					var filterValue = $(this).attr('data-filter');
+					$(".filters").find("li.active").removeClass("active");
+					$(this).parent().addClass("active");
+					$container.isotope({ filter: filterValue });
+					return false;
+				});
+			
+		};
+
+$timeout(init, 1000);
 }]);
