7f9fb08c74e2004f14dbece2267e2e0c87d40cef
komix <oleh.petrovskyi@gmail.com>
2015-01-21 17:00:44 +0200

    services page

--- app/portfolio/portfolio.js
+++ app/portfolio/portfolio.js
@@ -44,8 +44,28 @@ $scope.modal = function (_work) {
 }])
 
 
-.controller('ModalInstanceCtrl', function ($scope, $modalInstance, work) {
+.controller('ModalInstanceCtrl', function ($scope, $modalInstance, $animate, work) {
 
   $scope.work = work;
+
+$animate.enabled(false);
+$scope.myInterval = 5000;
+  var slides = $scope.slides = [{
+      image: work.imageUrl,
+      // text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+      //   ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+    }];
+  $scope.addSlide = function() {
+    var newWidth = 600 + slides.length + 1;
+    slides.push({
+      image: 'http://placekitten.com/' + newWidth + '/300',
+      text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+        ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+    });
+  };
+  for (var i=0; i<4; i++) {
+    $scope.addSlide();
+  }
+
 
 });
--- app/services/services.js
+++ app/services/services.js
@@ -9,6 +9,26 @@ angular.module('exel.services', ['ngRoute'])
   });
 }])
 
-.controller('ServicesController', [function() {
+.controller('ServicesController', ['$scope', "$http", function($scope, $http) {
+
+
+$http.get('services/services.json').success(function(data) {
+    $scope.services = data;
+});
+
+
+
+$(document).on('click', '.cat > .list-group-item', function(event) {
+        event.stopPropagation();
+        var $this = $(this);
+        var parent = $this.data('parent');
+        var actives = parent && $(parent).find('.collapse.in');
+        
+        if (actives && actives.length) {
+            actives.collapse('hide');
+        }
+});
+
+
 
 }]);
